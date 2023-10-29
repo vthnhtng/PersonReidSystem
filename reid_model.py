340e373c66bf8487b97ddc078514d4dcaef3763a
vthnhtng <tung5112002@gmail.com>
2023-10-30 00:09:11 +0700

    refactor and add are_same_person function

--- reid_model.py
+++ reid_model.py
@@ -23,90 +23,100 @@ def create_transform():
 
     return transform
 
+
+def L2_norm(tensor):
+    return torch.nn.functional.normalize(tensor, dim=1, p=2)
+
+
+def euclidean_distance(tensor1, tensor2): # use for L2 norm
+    diff = tensor1 - tensor2
+    distance = torch.sqrt(torch.sum(diff ** 2))
+    return distance
+
+def read_image(img_path):
+    """Keep reading image until succeed.
+    This can avoid IOError incurred by heavy IO process."""
+    got_img = False
+    if not osp.exists(img_path):
+        raise IOError("{} does not exist".format(img_path))
+    while not got_img:
+        try:
+            img = Image.open(img_path).convert('RGB')
+            got_img = True
+        except IOError:
+            print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+            pass
+    return img
+
+def cosine_similarity(tensor1, tensor2):
+    tensor1 = tensor1.detach().cpu().numpy()
+    tensor2 = tensor2.detach().cpu().numpy()
+
+    return cosine_similarity(tensor1, tensor2)
+
+
+def are_same_person(tensor1, tensor2, threshold):
+    tensor1_norm = L2_norm(tensor1)
+    tensor2_norm = L2_norm(tensor2)
+    dist = euclidean_distance(tensor1_norm, tensor2_norm)
+
+    return True if dist <= threshold else False
+
+
+def create_reid_model(pretrained_path, trained_path):
+    reid_model = Baseline(751, 1, pretrained_path, 'bnneck', 'after', 'resnet50', 'imagenet')
+
+    # Use io.BytesIO to pre-load the data into a buffer
+    with open(trained_path, 'rb') as f:
+        buffer = io.BytesIO(f.read())
+
+    param_dict = torch.load(buffer, map_location='cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    for i in param_dict:
+        if 'classifier' in i:
+            continue
+        reid_model.state_dict()[i].copy_(param_dict[i])
+
+    if torch.cuda.device_count() > 1:
+        reid_model = nn.DataParallel(model)
+
+    return reid_model
+
+
+def prepare_input_from_file(img_path, pid, camid, timestamp):
+    return PersonReidModel.transform(read_image(img_path)), pid, camid, timestamp
+
+
+def prepare_input_from_tensor(torch_tensor, pid, camid, timestamp):
+    return torch_tensor, pid, camid, timestamp
+
+    
 class PersonReidModel():
     transform = create_transform()
 
     def __init__(self, pretrained_path, trained_path):
-        self.model = self.create_reid_model(pretrained_path, trained_path)
+        self.model = create_reid_model(pretrained_path, trained_path)
         self.model.to('cuda')
         self.model.eval()
 
 
         # first inference took 2s to perform so put first inference to init 
-        foo_img_path = "./test1.jpg"
-        foo_data = self.prepare_input(foo_img_path, None, None)
-        self.perform_inference(foo_data)
-
-    @staticmethod
-    def create_reid_model(pretrained_path, trained_path):
-        reid_model = Baseline(751, 1, pretrained_path, 'bnneck', 'after', 'resnet50', 'imagenet')
-
-        # Use io.BytesIO to pre-load the data into a buffer
-        with open(trained_path, 'rb') as f:
-            buffer = io.BytesIO(f.read())
-
-
-        param_dict = torch.load(buffer, map_location='cuda' if torch.cuda.is_available() else 'cpu')
-
-
-        for i in param_dict:
-            if 'classifier' in i:
-                continue
-            reid_model.state_dict()[i].copy_(param_dict[i])
-
-        if torch.cuda.device_count() > 1:
-            reid_model = nn.DataParallel(model)
-
-        return reid_model
-
-    @staticmethod
-    def prepare_input(img_path, pid, camid):
-        # bounding_box = read_image(img_path)
-        # bounding_box = transform(bounding_box)
-        return PersonReidModel.transform(PersonReidModel.read_image(img_path)), pid, camid
-
-
+        foo_tensor = torch.empty((3, 123,384))
+        foo_input = [foo_tensor, None, None, None]
+        self.perform_inference(foo_input)
 
     #perform inference
+    #input là list gồm 4 phần tử lần lượt là 
+    #0: torch.tensor
+    #1: pid
+    #2: camid
+    #3: timestamp
     def perform_inference(self, input):
-        image_tensor = input[0]
+        image_tensor = input[0].unsqueeze(0).to('cuda')
         pid = input[1]
         camid = input[2]
-        image_tensor = image_tensor.unsqueeze(0).to('cuda')
+        timestamp = input[3]
         feature_tensor = self.model(image_tensor)
-        return feature_tensor, pid, camid
 
-    
-    @staticmethod
-    def cosine_similarity(tensor1, tensor2):
-        tensor1 = tensor1.detach().cpu().numpy()
-        tensor2 = tensor2.detach().cpu().numpy()
-
-        return cosine_similarity(tensor1, tensor2)
-
-    @staticmethod
-    def euclidean_distance(tensor1, tensor2): # use for L2 norm
-        diff = tensor1 - tensor2
-        distance = torch.sqrt(torch.sum(diff ** 2))
-        return distance
-
-
-    @staticmethod
-    def read_image(img_path):
-        """Keep reading image until succeed.
-        This can avoid IOError incurred by heavy IO process."""
-        got_img = False
-        if not osp.exists(img_path):
-            raise IOError("{} does not exist".format(img_path))
-        while not got_img:
-            try:
-                img = Image.open(img_path).convert('RGB')
-                got_img = True
-            except IOError:
-                print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
-                pass
-        return img
-
-    @staticmethod
-    def L2_norm(tensor):
-        return torch.nn.functional.normalize(tensor, dim=1, p=2)
+        return feature_tensor, pid, camid, timestamp
--- reid_model_test.py
+++ reid_model_test.py
@@ -1,3 +1,4 @@
+import reid_model
 from reid_model import PersonReidModel
 import time
 import torch
@@ -8,48 +9,35 @@ start = time.time()
 pretrained_path = "./pretrained_models/resnet50-19c8e357.pth"
 trained_path = "./trained_models/market_resnet50_model_120_rank1_945.pth"
 
-reid_model = PersonReidModel(pretrained_path, trained_path)
+model = PersonReidModel(pretrained_path, trained_path)
 end = time.time()
 print("init model time:" + str(end - start) + "s")
 
-img_path1 = './test1.jpg'
-img_path2 = './test2.jpg'
+img_path1 = './img/23.jpg'
+img_path2 = './img/27.jpg'
 pid = 1
 camid = 1
+timestamp = time.time()
 
 start = time.time()
-data1 = reid_model.prepare_input(img_path1, pid, camid)
-data2 = reid_model.prepare_input(img_path2, pid, camid)
+data1 = reid_model.prepare_input_from_file(img_path1, pid, camid, timestamp) # return tensor, pid, camid
+data2 = reid_model.prepare_input_from_file(img_path2, pid, camid, timestamp)
 end = time.time()
 print("prepare data time:" + str(end - start) + "s")
 
 # Perform inference
 start = time.time()
-feat1 = reid_model.perform_inference(data1)
-feat2 = reid_model.perform_inference(data2)
-
-feat1 = reid_model.L2_norm(feat1[0])
-feat2 = reid_model.L2_norm(feat2[0])
-
+output1 = model.perform_inference(data1)
+output2 = model.perform_inference(data2)
 end = time.time()
 
 
 print("inference time:" + str(end - start) + "s")
 
 # Calculate cosine similarity
-
-start = time.time()
-distance = reid_model.euclidean_distance(feat1, feat2)
-end = time.time()
-
-print("calc euclide time:" + str(end - start) + "s")
 threshold = 1.05
 
-print(distance)
-if distance >= threshold:
-  print("are not same person")
-elif distance < threshold:
-  print("are same person")
-
-
-
+if reid_model.are_same_person(output1[0], output2[0], threshold):
+  print("Are same person")
+else:
+  print("Are not same person")
